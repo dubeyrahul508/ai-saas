6b1895f2c0490df9041dd07b32f19221580faa54
Rahul Dubey <dubeyrahul508@gmail.com>
2025-09-06 23:44:31 +0100

    convex backend added

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,15 +1,19 @@
-import { Button } from "@workspace/ui/components/button"
-import { Input } from "@workspace/ui/components/input"
+"use client";
+
+import {useMutation, useQuery} from "convex/react";
+import {api} from "@workspace/backend/_generated/api";
+import { Button } from "@workspace/ui/components/button";
 
 export default function Page() {
+  const users = useQuery(api.users.getMany);
+  const add = useMutation(api.users.add);
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Widget</h1>
-        <Button size="sm">Button</Button>
-        <Input
-          placeholder="Input"
-        />
+        <h1 className="text-2xl font-bold">Hello World</h1>
+        <p>/widget</p>
+        <Button onClick={() => add()}>Add Dubey</Button>
+        {JSON.stringify(users)}
       </div>
     </div>
   )
